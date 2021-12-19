a052802292ad4cecd270650bf29c6b1f12341213
Simpe93 <simon93swe@gmail.com>
2021-12-19 18:10:04 +0100

    Moved faceit stuff back to faceit.js

--- js/common/faceit.js
+++ js/common/faceit.js
@@ -1,18 +1,37 @@
-async function getFaceitLevel(data) {
-    //if (!userStorage.settings.faceitlevels) {
-        if (data) {
-            if (data.payload.players.results.length != 0 && result.payload.players.results[0].games.length != 0) {
-                games = data.payload.players.results[0].games[0];
-            } else {
-                games = "";
-            }
-            if (!games || games == "") {
-                return ["", ""];
-            } else if (games.name == "csgo") {
-                return [games.skill_level, result.payload.players.results[0].nickname];
-            }
+function setFaceitLevel(data) {
+    const levelCols = {
+        1: "#ededed",
+        2: "#1ce200",
+        3: "#1ce200",
+        4: "#fec700",
+        5: "#fec700",
+        6: "#fec700",
+        7: "#fec700",
+        8: "#ff6309",
+        9: "#ff6309",
+        10: "#f91e00"
+    }
+
+    if (data) {
+        if (data.payload.players.results.length != 0 && data.payload.players.results[0].games.length != 0) {
+            games = data.payload.players.results[0].games[0];
+        } else {
+            games = "";
         }
-    //} else {
-    //    return ["", ""];
-    //}
+        if (!games || games == "") {
+            return ["", ""];
+        } else if (games.name == "csgo") {
+            level = games.skill_level
+            nick = data.payload.players.results[0].nickname;
+            setTimeout(function () {
+                wrapper = document.querySelector(".user-profile-rank-rating").querySelectorAll(".section")[0];
+                span = document.createElement("a");
+                span.innerHTML = level;
+                span.href = `https://faceit.com/en/players/${nick}`;
+                span.target = "_BLANK";
+                span.style.cssText = `color: ${levelCols[level]};margin-left:10px;border-radius:50%;padding:3px 8px;border:2px solid ${levelCols[level]}`;
+                wrapper.appendChild(span);
+            }, 1000);
+        }
+    }
 }
--- js/content.js
+++ js/content.js
@@ -127,41 +127,7 @@ chrome.runtime.onMessage.addListener(
                 }
             });
         } else if (request.message === 'profilePage') {
-            const levelCols = {
-                1: "#ededed",
-                2: "#1ce200",
-                3: "#1ce200",
-                4: "#fec700",
-                5: "#fec700",
-                6: "#fec700",
-                7: "#fec700",
-                8: "#ff6309",
-                9: "#ff6309",
-                10: "#f91e00"
-            }
-
-            if (request.data) {
-                if (request.data.payload.players.results.length != 0 && request.data.payload.players.results[0].games.length != 0) {
-                    games = request.data.payload.players.results[0].games[0];
-                } else {
-                    games = "";
-                }
-                if (!games || games == "") {
-                    return ["", ""];
-                } else if (games.name == "csgo") {
-                    level = games.skill_level
-                    nick = request.data.payload.players.results[0].nickname;
-                    setTimeout(function () {
-                        wrapper = document.querySelector(".user-profile-rank-rating").querySelectorAll(".section")[0];
-                        span = document.createElement("a");
-                        span.innerHTML = level;
-                        span.href = `https://faceit.com/en/players/${nick}`;
-                        span.target = "_BLANK";
-                        span.style.cssText = `color: ${levelCols[level]};margin-left:10px;border-radius:50%;padding:3px 8px;border:2px solid ${levelCols[level]}`;
-                        wrapper.appendChild(span);
-                    }, 1000);
-                }
-            }
+            setFaceitLevel(request.data);
         }
     }
 );
